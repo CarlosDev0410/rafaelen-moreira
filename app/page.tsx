ba2a8e6ffe7b32362cda783d7a9c813552633977
Cadu <kadu0410@gmail.com>
2026-02-07 16:46:17 -0300

    Feature: SpeedInsights vercel

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import Specialties from "./components/Specialties";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <main className="min-h-screen relative selection:bg-primary-light selection:text-primary-dark">
+      <SpeedInsights/>
       <Header />
       
       <Hero />
